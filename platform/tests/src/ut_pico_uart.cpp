7b77862aa2cc9cbf10dbfe701d2458575ea97d5b
fedddot <idanrommm@gmail.com>
2024-07-31 21:23:18 +0300

    test updated

--- platform/tests/src/ut_pico_uart.cpp
+++ platform/tests/src/ut_pico_uart.cpp
@@ -7,5 +7,17 @@ using namespace pico_mcu_platform;
 int main(void) {
 	PicoUart uart("abc", "efg", 100, PicoUart::Baud::BAUD9600);
 
+	if (uart.message_received()) {
+		return -1;
+	}
+
+	uart.send("hahaha"); // TX is closed on RX
+	if (!uart.message_received()) {
+		return -1;
+	}
+	if ("hahaha" != uart.receive()) {
+		return -1;
+	}
+
 	return 0;
 }
