bb463b3209e6343a3441cc7bf8298c61a864377b
fedddot <idanrommm@gmail.com>
2024-07-31 20:29:42 +0300

    redundants deleted

--- platform/tests/src/ut_pico_gpi.cpp
+++ platform/tests/src/ut_pico_gpi.cpp
@@ -1,9 +1,18 @@
-#include "gtest/gtest.h"
-
 #include "pico_gpi.hpp"
 
+using namespace pico_mcu_platform;
+
 TEST(ut_pico_gpi, ctor_dtor_sanity) {
+	// GIVEN
+	const int gpi_id(2);
+	
 	// WHEN
+	PicoGpi *instance_ptr(nullptr);
 
 	// THEN
+	ASSERT_NO_THROW(
+		instance_ptr = new PicoGpi(gpi_id);
+	);
+	ASSERT_NO_THROW(delete instance_ptr);
+	instance_ptr = nullptr;
 }
