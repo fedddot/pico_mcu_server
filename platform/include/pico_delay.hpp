e331a975ca634da60c38923d7859954713033e86
fedddot <idanrommm@gmail.com>
2024-08-01 20:22:39 +0300

    pico delay updated

--- platform/include/pico_delay.hpp
+++ platform/include/pico_delay.hpp
@@ -1,31 +1,28 @@
 #ifndef	PICO_DELAY_HPP
 #define	PICO_DELAY_HPP
 
-#include <memory>
-
-#include "pico/stdlib.h"
+#include "pico/time.h"
 
 #include "delay.hpp"
 
 namespace pico_mcu_server {
 
-	class PicoDelay: public mcu_task_engine::Delay {
+	class PicoDelay: public mcu_platform::Delay {
 	public:
-		PicoDelay(int delay_ms);
-		PicoDelay(const PicoDelay& other) = delete;
+		PicoDelay() = default;
+		PicoDelay(const PicoDelay& other) = default;
 		PicoDelay& operator=(const PicoDelay& other) = delete;
 
-		void delay() const override;
-	private:
-		int m_delay_ms;
+		void delay(int delay_ms) const override;
+		mcu_platform::Delay *clone() const override;
 	};
 
-	inline PicoDelay::PicoDelay(int delay_ms): m_delay_ms(delay_ms) {
-
+	inline void PicoDelay::delay(int delay_ms) const {
+		sleep_ms(delay_ms);
 	}
 
-	inline void PicoDelay::delay() const {
-		sleep_ms(m_delay_ms);
+	inline mcu_platform::Delay *PicoDelay::clone() const {
+		return new PicoDelay(*this);
 	}
 }
 
