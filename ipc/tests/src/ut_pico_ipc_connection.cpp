791fa6baa67f734230f357d12b6261b4130d8da9
fedddot <idanrommm@gmail.com>
2024-08-02 12:35:41 +0300

    test updated

--- ipc/tests/src/ut_pico_ipc_connection.cpp
+++ ipc/tests/src/ut_pico_ipc_connection.cpp
@@ -1,24 +1,34 @@
+#include <cstddef>
 #include <stdexcept>
 
+#include "pico/time.h"
 #include "pico_ipc_connection.hpp"
 
 using namespace pico_mcu_ipc;
 
 int main(void) {
 	// GIVEN
-	const int gpi_id(2);
-	
+	const PicoIpcConnection::Baud baud(PicoIpcConnection::Baud::B9600);
+	const pico_mcu_ipc::PicoIpcData head("MSG_HEADER");
+	const pico_mcu_ipc::PicoIpcData data("test_data");
+	const pico_mcu_ipc::PicoIpcData tail("MSG_TAIL");
+	const std::size_t max_size(1000UL);
+
 	// WHEN
 	PicoIpcConnection *instance_ptr(nullptr);
 
 	// THEN
 	try {
-		instance_ptr = new PicoIpcConnection();
-		if (nullptr == instance_ptr) {
-			throw std::runtime_error("");
+		PicoIpcConnection instance(baud, head, tail, max_size);
+		while (true) {
+			if (!instance.readable()) {
+				continue;
+			}
+			auto incomming_data = instance.read();
+			instance.send(incomming_data);
 		}
-		delete instance_ptr;
-		instance_ptr = nullptr;
+
+
 	} catch (...) {
 		return -1;
 	}
