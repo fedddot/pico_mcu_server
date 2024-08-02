788a0e932231403ac567bbc0f934989c81060efd
fedddot <idanrommm@gmail.com>
2024-08-02 11:19:38 +0300

    pico ipc updated

--- ipc/include/pico_ipc_connection.hpp
+++ ipc/include/pico_ipc_connection.hpp
@@ -2,6 +2,7 @@
 #define	PICO_IPC_CONNECTION_HPP
 
 #include "buffered_custom_ipc_connection.hpp"
+#include "hardware/gpio.h"
 #include "hardware/uart.h"
 #include "ipc_connection.hpp"
 #include <stdexcept>
@@ -39,16 +40,32 @@ namespace pico_mcu_ipc {
 		static CustomConnection *s_connection;
 		static void on_received_cb();
 		static void send_data(const PicoIpcData& data);
+		static uint baud_to_uint(const Baud& baud);
 	};
 
 	inline PicoIpcConnection::PicoIpcConnection(const Baud& baud, const PicoIpcData& head, const PicoIpcData& tail, const std::size_t& max_buff_size): m_connection(head, tail, max_buff_size, send_data) {
 		if (nullptr != s_connection) {
 			throw std::runtime_error("uart0 connection is already created");
 		}
+		uart_init(uart0, baud_to_uint(baud));
+		gpio_set_function(UART0_TX_PIN, GPIO_FUNC_UART);
+    	gpio_set_function(UART0_RX_PIN, GPIO_FUNC_UART);
+		uart_set_baudrate(uart0, baud_to_uint(baud));
+		uart_set_hw_flow(uart0, false, false);
+		uart_set_format(uart0, DATA_BITS, STOP_BITS, UART_PARITY_NONE);
+		uart_set_fifo_enabled(uart0, false);
+	    irq_set_exclusive_handler(UART0_IRQ, &on_received_cb);
+    	irq_set_enabled(UART0_IRQ, true);
+    	uart_set_irq_enables(uart0, true, false);
 		s_connection = &m_connection;
 	}
 
 	inline PicoIpcConnection::~PicoIpcConnection() noexcept {
+		irq_set_enabled(UART0_IRQ, false);
+    	uart_set_irq_enables(uart0, false, false);
+    	uart_deinit(uart0);
+		gpio_set_function(UART0_TX_PIN, GPIO_FUNC_NULL);
+    	gpio_set_function(UART0_RX_PIN, GPIO_FUNC_NULL);
 		s_connection = nullptr;
 	}
 
@@ -78,6 +95,17 @@ namespace pico_mcu_ipc {
 	inline void PicoIpcConnection::send_data(const PicoIpcData& data) {
 		for (auto ch: data) {
 			uart_putc(uart0, ch);
+		}
+	}
+
+	inline uint PicoIpcConnection::baud_to_uint(const Baud& baud) {
+		switch (baud) {
+		case Baud::B9600:
+			return 9600;
+		case Baud::B115200:
+			return 115200;
+		default:
+			throw std::invalid_argument("unsupported baud");
 		}
 	}
 }
