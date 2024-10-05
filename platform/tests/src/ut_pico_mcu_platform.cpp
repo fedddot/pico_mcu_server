2c8b0feff87e46f541686405834c4adcd48b51ae
fedddot <idanrommm@gmail.com>
2024-10-05 11:27:35 +0300

    platform updated

--- platform/tests/src/ut_pico_mcu_platform.cpp
+++ platform/tests/src/ut_pico_mcu_platform.cpp
@@ -1,24 +1,41 @@
 #include <stdexcept>
 
-#include "pico_mcu_platform.hpp"
+#include "pico_gpi.hpp"
+#include "pico_gpo.hpp"
 
 using namespace pico_mcu_platform;
+
+template <typename T>
+inline void assert_eq(const T& one, const T& other) {
+	if (one != other) {
+		throw std::runtime_error("equality assertion failed");
+	}
+}
+
+template <typename T>
+inline void assert_ne(const T& one, const T& other) {
+	if (one == other) {
+		throw std::runtime_error("inequality assertion failed");
+	}
+}
 
 int main(void) {
 	// GIVEN
 	const int gpi_id(2);
+	const int gpo_id(2);
 	
 	// WHEN
-	PicoMcuPlatform *instance_ptr(nullptr);
+	PicoGpi *gpi_ptr(nullptr);
+	PicoGpo *gpo_ptr(nullptr);
 
 	// THEN
 	try {
-		instance_ptr = new PicoMcuPlatform();
-		if (nullptr == instance_ptr) {
-			throw std::runtime_error("");
-		}
-		delete instance_ptr;
-		instance_ptr = nullptr;
+		gpi_ptr = new PicoGpi(gpi_id);
+		assert_ne(static_cast<PicoGpi *>(nullptr), gpi_ptr);
+		assert_eq(PicoGpi::Direction::IN, gpi_ptr->direction());
+		assert_eq(PicoGpi::State::LOW, gpi_ptr->state());
+		delete gpi_ptr;
+		gpi_ptr = nullptr;
 	} catch (...) {
 		return -1;
 	}
