7feaa88f96bbfef3eab34512c031815c51ce9290
fedddot <idanrommm@gmail.com>
2024-10-06 23:12:21 +0300

    proxy updated

--- uart_proxy/uart_proxy.py
+++ uart_proxy/uart_proxy.py
@@ -1,9 +1,71 @@
 import serial
 import time
+import sys
+import json
+import re
 
-ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=100)
-print(ser.name)
-ser.write(b'MSG_HEAD{\"method\": \"CREATE\", \"path\":\"gpios\", \"body\": {\"id\": \"10\", \"dir\": 0}}MSG_TAIL')
-time.sleep(5)
-print(ser.read_all())
+def get_param_from_args(param, args):
+    prefix = "--{}=".format(param)
+    for arg in args:
+        if not arg.startswith(prefix):
+            continue
+        return arg[len(prefix):]
+    raise Exception("param {} is missing")
+
+port = get_param_from_args("port", sys.argv)
+baud = int(get_param_from_args("baud", sys.argv))
+ser = serial.Serial(
+    port,
+    baud,
+    timeout=100
+)
+print("opened port: {}, baud {}".format(port, baud))
+
+create_request = {
+    "method": "CREATE",
+    "path": "gpios",
+    "body": {
+        "id": "25",
+        "dir": 1
+    }
+}
+update_high_request = {
+    "method": "UPDATE",
+    "path": "gpios/25",
+    "body": {
+        "state": 1
+    }
+}
+update_low_request = {
+    "method": "UPDATE",
+    "path": "gpios/25",
+    "body": {
+        "state": 0
+    }
+}
+delete_request = {
+    "method": "DELETE",
+    "path": "gpios/25",
+    "body": {}
+}
+
+def run_request(ser, request):
+    str_request = json.dumps(request)
+    print("sending request to the server:\n{}".format(str_request))
+    ser.write(("MSG_HEAD" + str_request + "MSG_TAIL").encode("utf-8"))
+    response_str = ""
+    while True:
+        new_data = ser.read_all().decode("utf-8")
+        response_str += new_data
+        if "MSG_HEAD" in response_str and "MSG_TAIL" in response_str:
+            break;
+    extracted_response = re.search('MSG_HEAD(.*)MSG_TAIL', response_str).group(1)
+    print("received extracted report: {}".format(extracted_response))
+    return json.loads(extracted_response)
+
+run_request(ser, create_request)
+run_request(ser, update_high_request)
+run_request(ser, update_low_request)
+run_request(ser, delete_request)
+
 ser.close()
