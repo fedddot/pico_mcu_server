2afd90597f101d6e0aa225be1dadfb94501428a8
fedddot <idanrommm@gmail.com>
2024-10-06 23:38:50 +0300

    proxy updated

--- uart_proxy/uart_proxy.py
+++ uart_proxy/uart_proxy.py
@@ -2,7 +2,7 @@ import serial
 import time
 import sys
 import json
-import re
+from uart_request_handler import UartRequestHandler
 
 def get_param_from_args(param, args):
     prefix = "--{}=".format(param)
@@ -12,14 +12,10 @@ def get_param_from_args(param, args):
         return arg[len(prefix):]
     raise Exception("param {} is missing")
 
-port = get_param_from_args("port", sys.argv)
-baud = int(get_param_from_args("baud", sys.argv))
-ser = serial.Serial(
-    port,
-    baud,
-    timeout=100
+uart_handler = UartRequestHandler(
+    port = get_param_from_args("port", sys.argv),
+    baud = int(get_param_from_args("baud", sys.argv))
 )
-print("opened port: {}, baud {}".format(port, baud))
 
 create_request = {
     "method": "CREATE",
@@ -43,29 +39,26 @@ update_low_request = {
         "state": 0
     }
 }
+read_request = {
+    "method": "READ",
+    "path": "gpios/25",
+    "body": {}
+}
 delete_request = {
     "method": "DELETE",
     "path": "gpios/25",
     "body": {}
 }
 
-def run_request(ser, request):
-    str_request = json.dumps(request)
-    print("sending request to the server:\n{}".format(str_request))
-    ser.write(("MSG_HEAD" + str_request + "MSG_TAIL").encode("utf-8"))
-    response_str = ""
-    while True:
-        new_data = ser.read_all().decode("utf-8")
-        response_str += new_data
-        if "MSG_HEAD" in response_str and "MSG_TAIL" in response_str:
-            break;
-    extracted_response = re.search('MSG_HEAD(.*)MSG_TAIL', response_str).group(1)
-    print("received extracted report: {}".format(extracted_response))
-    return json.loads(extracted_response)
+def run_request(handler, request):
+    print("\nrunning request:\n{}".format(request))
+    response = handler.run_request(request)
+    print("received response:\n{}".format(response))
 
-run_request(ser, create_request)
-run_request(ser, update_high_request)
-run_request(ser, update_low_request)
-run_request(ser, delete_request)
-
-ser.close()
+run_request(uart_handler, create_request)
+run_request(uart_handler, read_request)
+run_request(uart_handler, update_high_request)
+run_request(uart_handler, read_request)
+run_request(uart_handler, update_low_request)
+run_request(uart_handler, read_request)
+run_request(uart_handler, delete_request)
