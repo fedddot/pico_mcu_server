591da64defeb2a7fc1f8dbf6c48e599d1a5c953b
fedddot <idanrommm@gmail.com>
2024-10-06 18:29:50 +0300

    updated

--- ipc/tests/src/ut_pico_ipc_connection.cpp
+++ ipc/tests/src/ut_pico_ipc_connection.cpp
@@ -1,31 +1,65 @@
-#include <cstddef>
+#include <stdexcept>
+#include <string>
 
 #include "pico_ipc_connection.hpp"
+#include "json_request_parser.hpp"
+#include "json_response_serializer.hpp"
+#include "request.hpp"
+#include "response.hpp"
 
 using namespace pico_mcu_ipc;
+using namespace server_utl;
 
 int main(void) {
 	// GIVEN
 	const PicoIpcConnection::Baud baud(PicoIpcConnection::Baud::B9600);
-	const pico_mcu_ipc::PicoIpcData head("MSG_HEADER");
-	const pico_mcu_ipc::PicoIpcData data("test_data");
-	const pico_mcu_ipc::PicoIpcData tail("MSG_TAIL");
-	const std::size_t max_size(1000UL);
+	const RawData head("head");
+	const RawData tail("tail");
+
+	auto matcher = [head, tail](const RawData& data) {
+		auto head_pos = data.find(head);
+		if (RawData::npos == head_pos) {
+			return false;
+		}
+		auto tail_pos = data.find(tail, head_pos + head.size());
+		if (RawData::npos == tail_pos) {
+			return false;
+		}
+		return true;
+	};
+	auto extractor = [head, tail](RawData *data) {
+		auto head_pos = data->find(head);
+		if (RawData::npos == head_pos) {
+			throw std::invalid_argument("missing head");
+		}
+		auto tail_pos = data->find(tail, head_pos + head.size());
+		if (RawData::npos == tail_pos) {
+			throw std::invalid_argument("missing tail");
+		}
+		RawData extracted(data->begin() + head_pos + head.size(), data->begin() + tail_pos);
+		data->erase(data->begin() + head_pos, data->begin() + tail_pos + tail.size());
+		return JsonRequestParser()(extracted);
+	};
+	auto serializer = [head, tail](const server::Response& response) {
+		return head + JsonResponseSerializer()(response) + tail;
+	};
 
 	// WHEN
 	PicoIpcConnection *instance_ptr(nullptr);
 
 	// THEN
 	try {
-		PicoIpcConnection instance(baud, head, tail, max_size);
-		while (true) {
-			if (!instance.readable()) {
-				continue;
+		PicoIpcConnection instance(baud, serializer, matcher, extractor);
+		instance.subscribe(
+			"test_subsc",
+			[&instance](const server::Request& req) {
+				instance.send(server::Response(server::Response::ResponseCode::OK, req.body()));
 			}
-			auto incomming_data = instance.read();
-			instance.send(incomming_data);
-		}
+		);
 
+		while (true) {
+			
+		}
 
 	} catch (...) {
 		return -1;
