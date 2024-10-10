22bdc11e108cff8f3e192da47625692eccd37cf1
IRom <Idan.Rom@harman.com>
2024-10-10 22:53:55 +0300

    ut added

---
+++ platform/tests/src/ut_pico_stepper_motor.cpp
@@ -0,0 +1,45 @@
+#include <stdexcept>
+
+#include "pico_gpi.hpp"
+#include "pico_gpo.hpp"
+#include "pico_stepper_motor.hpp"
+
+using namespace pico_mcu_platform;
+
+template <typename T>
+inline void assert_eq(const T& one, const T& other) {
+	if (one != other) {
+		throw std::runtime_error("equality assertion failed");
+	}
+}
+
+template <typename T>
+inline void assert_ne(const T& one, const T& other) {
+	if (one == other) {
+		throw std::runtime_error("inequality assertion failed");
+	}
+}
+
+int main(void) {
+	// GIVEN
+	const int gpi_id(2);
+	const int gpo_id(2);
+	
+	// WHEN
+	PicoGpi *gpi_ptr(nullptr);
+	PicoGpo *gpo_ptr(nullptr);
+
+	// THEN
+	try {
+		gpi_ptr = new PicoGpi(gpi_id);
+		assert_ne(static_cast<PicoGpi *>(nullptr), gpi_ptr);
+		assert_eq(PicoGpi::Direction::IN, gpi_ptr->direction());
+		assert_eq(PicoGpi::State::LOW, gpi_ptr->state());
+		delete gpi_ptr;
+		gpi_ptr = nullptr;
+	} catch (...) {
+		return -1;
+	}
+
+	return 0;
+}
