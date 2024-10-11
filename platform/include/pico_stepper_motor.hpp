182af64b8bd32cc26f07cdb8dba2664d2fd44686
IRom <Idan.Rom@harman.com>
2024-10-11 10:41:21 +0300

    updated

--- platform/include/pico_stepper_motor.hpp
+++ platform/include/pico_stepper_motor.hpp
@@ -1,23 +1,56 @@
 #ifndef	PICO_STEPPER_MOTOR_HPP
 #define	PICO_STEPPER_MOTOR_HPP
 
+#include <array>
+#include <cstddef>
+#include <map>
+#include <memory>
 #include <stdexcept>
 
+#include "gpio.hpp"
+#include "gpo.hpp"
+#include "pico_gpo.hpp"
 #include "stepper_motor.hpp"
 
 namespace pico_mcu_platform {
 
 	class PicoStepperMotor: public manager::StepperMotor {
 	public:
-		PicoStepperMotor() = default;
+		enum class Shoulder: int {
+			LEFT_HIGH,
+			LEFT_LOW,
+			RIGHT_HIGH,
+			RIGHT_LOW
+		};
+		using ShouldersMapping = std::map<Shoulder, unsigned int>;
+		PicoStepperMotor(const ShouldersMapping& shoulders);
 		PicoStepperMotor(const PicoStepperMotor& other) = default;
 		PicoStepperMotor& operator=(const PicoStepperMotor& other) = delete;
 		void steps(const Direction& direction, unsigned int steps_num, unsigned int step_duration_ms) override;
 		manager::StepperMotor *clone() const override;
+	private:
+		using Shoulders = std::map<Shoulder, std::unique_ptr<manager::Gpo>>;
+		using GpioState = typename manager::Gpio::State;
+		using State = std::map<Shoulder, GpioState>;
+		enum: std::size_t {STATES_NUM = 8};
+		using States = std::array<State, STATES_NUM>;
+
+		Shoulders m_shoulders;
+		static const States s_states;
+		std::size_t m_current_state_number;
+
+		static std::size_t get_next_state(const std::size_t& from, const Direction& direction);
+		void apply_state(const State& state);
 	};
 
+	inline PicoStepperMotor::PicoStepperMotor(const ShouldersMapping& shoulders): m_current_state_number(0UL) {
+		for (const auto& [shoulder, gpo_num]: shoulders) {
+			m_shoulders.insert({shoulder, std::unique_ptr<manager::Gpo>(new PicoGpo(gpo_num))});
+		}
+	}
+
 	inline void PicoStepperMotor::steps(const Direction& direction, unsigned int steps_num, unsigned int step_duration_ms) {
-		throw std::runtime_error("NOT_IMPLEMENTED");
+		
 	}
 
 	inline manager::StepperMotor *PicoStepperMotor::clone() const {
