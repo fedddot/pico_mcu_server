be9659c28bbe6417e6dc439e78fc32f73b1e80e5
fedddot <idanrommm@gmail.com>
2024-10-11 11:14:18 +0300

    motor updated

--- platform/include/pico_stepper_motor.hpp
+++ platform/include/pico_stepper_motor.hpp
@@ -26,7 +26,7 @@ namespace pico_mcu_platform {
 		PicoStepperMotor(const ShouldersMapping& shoulders);
 		PicoStepperMotor(const PicoStepperMotor& other) = default;
 		PicoStepperMotor& operator=(const PicoStepperMotor& other) = delete;
-		void steps(const Direction& direction, unsigned int steps_num, unsigned int step_duration_ms) override;
+		void steps(const Direction& direction, const unsigned int steps_num, const unsigned int step_duration_ms) override;
 		manager::StepperMotor *clone() const override;
 	private:
 		using Shoulders = std::map<Shoulder, std::unique_ptr<manager::Gpo>>;
@@ -37,6 +37,7 @@ namespace pico_mcu_platform {
 
 		Shoulders m_shoulders;
 		static const States s_states;
+		static const State s_shutdown_state;
 		std::size_t m_current_state_number;
 
 		static std::size_t get_next_state(const std::size_t& from, const Direction& direction);
@@ -47,14 +48,51 @@ namespace pico_mcu_platform {
 		for (const auto& [shoulder, gpo_num]: shoulders) {
 			m_shoulders.insert({shoulder, std::unique_ptr<manager::Gpo>(new PicoGpo(gpo_num))});
 		}
+		apply_state(s_shutdown_state);
 	}
 
-	inline void PicoStepperMotor::steps(const Direction& direction, unsigned int steps_num, unsigned int step_duration_ms) {
-		
+	inline void PicoStepperMotor::steps(const Direction& direction, const unsigned int steps_num, const unsigned int step_duration_ms) {
+		auto steps_to_go(steps_num);
+		while (steps_to_go) {
+			auto next_state = get_next_state(m_current_state_number, direction);
+			apply_state(s_states[next_state]);
+			m_current_state_number = next_state;
+			--steps_to_go;
+		}
 	}
 
 	inline manager::StepperMotor *PicoStepperMotor::clone() const {
 		return new PicoStepperMotor(*this);
+	}
+
+	inline std::size_t PicoStepperMotor::get_next_state(const std::size_t& from, const Direction& direction) {
+		auto get_next_cw = [](const std::size_t& from) {
+			auto next(from + 1);
+			if (s_states.size() <= next) {
+				return std::size_t(0UL);
+			}
+			return next;
+		};
+		auto get_next_ccw = [](const std::size_t& from) {
+			if (0UL == from) {
+				return s_states.size() - 1UL;
+			}
+			return from - 1UL;
+		};
+		switch (direction) {
+		case Direction::CW:
+			return get_next_cw(from);
+		case Direction::CCW:
+			return get_next_ccw(from);
+		default:
+			throw std::invalid_argument("invalid direction received");
+		}
+	}
+
+	inline void PicoStepperMotor::apply_state(const State& state) {
+		for (const auto& [shoulder, gpo_state]: state) {
+			m_shoulders[shoulder]->set_state(gpo_state);
+		}
 	}
 }
 
