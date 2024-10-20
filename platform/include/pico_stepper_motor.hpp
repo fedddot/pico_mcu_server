11b845a56200b1f9b5de3f7f95cb2b92e4a6967b
fedddot <idanrommm@gmail.com>
2024-10-20 12:15:29 +0300

    stepper motor updated

--- platform/include/pico_stepper_motor.hpp
+++ platform/include/pico_stepper_motor.hpp
@@ -5,7 +5,6 @@
 #include <cstddef>
 #include <map>
 #include <memory>
-#include <pico/time.h>
 #include <stdexcept>
 
 #include "gpio.hpp"
@@ -25,41 +24,51 @@ namespace pico_mcu_platform {
 		};
 		using ShouldersMapping = std::map<Shoulder, unsigned int>;
 		PicoStepperMotor(const ShouldersMapping& shoulders, const unsigned int enable);
-		PicoStepperMotor(const PicoStepperMotor& other) = default;
+		PicoStepperMotor(const PicoStepperMotor& other) = delete;
 		PicoStepperMotor& operator=(const PicoStepperMotor& other) = delete;
-		void steps(const Direction& direction, const unsigned int steps_num, const unsigned int on_time, const unsigned int off_time) override;
+
+		void enable() override;
+		void disable() override;
+		bool enabled() const override;
+		void step(const Direction& direction) override;
 	private:
-		using Shoulders = std::map<Shoulder, std::shared_ptr<manager::Gpo>>;
+		using Shoulders = std::map<Shoulder, std::unique_ptr<manager::Gpo>>;
 		using GpioState = typename manager::Gpio::State;
 		using State = std::map<Shoulder, GpioState>;
 		enum: std::size_t {STATES_NUM = 8};
 		using States = std::array<State, STATES_NUM>;
 
 		Shoulders m_shoulders;
-		std::shared_ptr<manager::Gpo> m_enable;
+		std::unique_ptr<manager::Gpo> m_enable;
 
 		static const States s_states;
 		std::size_t m_current_state_number;
 
 		static std::size_t get_next_state(const std::size_t& from, const Direction& direction);
-		void apply_state(const State& state, const unsigned int on_time, const unsigned int off_time);
+		void apply_state(const State& state);
 	};
 
 	inline PicoStepperMotor::PicoStepperMotor(const ShouldersMapping& shoulders, const unsigned int enable): m_current_state_number(0UL), m_enable(new PicoGpo(enable)) {
 		for (const auto& [shoulder, gpo_num]: shoulders) {
-			m_shoulders.insert({shoulder, std::shared_ptr<manager::Gpo>(new PicoGpo(gpo_num))});
+			m_shoulders.insert({shoulder, std::unique_ptr<manager::Gpo>(new PicoGpo(gpo_num))});
 		}
+	}
+
+	inline void PicoStepperMotor::enable() {
+		m_enable->set_state(GpioState::HIGH);
+	}
+
+	inline void PicoStepperMotor::disable() {
 		m_enable->set_state(GpioState::LOW);
 	}
 
-	inline void PicoStepperMotor::steps(const Direction& direction, const unsigned int steps_num, const unsigned int on_time, const unsigned int off_time) {
-		auto steps_to_go(steps_num);
-		while (steps_to_go) {
-			auto next_state = get_next_state(m_current_state_number, direction);
-			apply_state(s_states[next_state], on_time, off_time);
-			m_current_state_number = next_state;
-			--steps_to_go;
-		}
+	inline bool PicoStepperMotor::enabled() const {
+		return GpioState::HIGH == m_enable->state();
+	}
+
+	inline void PicoStepperMotor::step(const Direction& direction) {
+		auto next_state = get_next_state(m_current_state_number, direction);
+		apply_state(s_states[next_state]);
 	}
 
 	inline std::size_t PicoStepperMotor::get_next_state(const std::size_t& from, const Direction& direction) {
@@ -86,15 +95,12 @@ namespace pico_mcu_platform {
 		}
 	}
 
-	inline void PicoStepperMotor::apply_state(const State& state, const unsigned int on_time, const unsigned int off_time) {
-		m_enable->set_state(GpioState::LOW);
+	inline void PicoStepperMotor::apply_state(const State& state) {
+		disable();
 		for (const auto& [shoulder, gpo_state]: state) {
 			m_shoulders[shoulder]->set_state(gpo_state);
 		}
-		m_enable->set_state(GpioState::HIGH);
-		sleep_us(on_time);
-		m_enable->set_state(GpioState::LOW);
-		sleep_us(off_time);
+		enable();
 	}
 }
 
--- platform/src/pico_stepper_motor.cpp
+++ platform/src/pico_stepper_motor.cpp
@@ -1,45 +1,10 @@
-#include "pico_stepper_motor.hpp"
 #include "gpio.hpp"
+#include "pico_stepper_motor.hpp"
 
 using namespace pico_mcu_platform;
 
 using Shoulder = typename PicoStepperMotor::Shoulder;
 using GpioState = typename manager::Gpio::State;
-
-// const PicoStepperMotor::States pico_mcu_platform::PicoStepperMotor::s_states {
-// 	PicoStepperMotor::State(
-// 		{
-// 			{Shoulder::A0,	GpioState::HIGH},
-// 			{Shoulder::A1,	GpioState::LOW},
-// 			{Shoulder::B0,	GpioState::LOW},
-// 			{Shoulder::B1,	GpioState::LOW}
-// 		}
-// 	),
-// 	PicoStepperMotor::State(
-// 		{
-// 			{Shoulder::A0,	GpioState::LOW},
-// 			{Shoulder::A1,	GpioState::LOW},
-// 			{Shoulder::B0,	GpioState::HIGH},
-// 			{Shoulder::B1,	GpioState::LOW}
-// 		}
-// 	),
-// 	PicoStepperMotor::State(
-// 		{
-// 			{Shoulder::A0,	GpioState::LOW},
-// 			{Shoulder::A1,	GpioState::HIGH},
-// 			{Shoulder::B0,	GpioState::LOW},
-// 			{Shoulder::B1,	GpioState::LOW}
-// 		}
-// 	),
-// 	PicoStepperMotor::State(
-// 		{
-// 			{Shoulder::A0,	GpioState::LOW},
-// 			{Shoulder::A1,	GpioState::LOW},
-// 			{Shoulder::B0,	GpioState::LOW},
-// 			{Shoulder::B1,	GpioState::HIGH}
-// 		}
-// 	)
-// };
 
 const PicoStepperMotor::States pico_mcu_platform::PicoStepperMotor::s_states {
 	PicoStepperMotor::State(
