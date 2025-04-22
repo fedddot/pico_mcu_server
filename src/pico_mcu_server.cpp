597c82ea0f84fbeaa8d2816bfac733a47b072f4a
fedddot <idanrommm@gmail.com>
2025-04-22 14:47:30 +0000

    mcu_server updated

--- src/pico_mcu_server.cpp
+++ src/pico_mcu_server.cpp
@@ -1,144 +1,68 @@
-#include <cstdint>
-#include <stdexcept>
+#include <cstddef>
 #include <string>
 
 #include "pico/stdio.h"
 #include "pico/time.h"
 
-#include "cnc_server.hpp"
-#include "data.hpp"
-#include "gpio.hpp"
-#include "integer.hpp"
-#include "json_request_parser.hpp"
-#include "json_response_serializer.hpp"
-#include "object.hpp"
-#include "pico_gpi.hpp"
-#include "pico_gpo.hpp"
-#include "pico_synchronous_ipc_connection.hpp"
-#include "request.hpp"
-#include "server_exception.hpp"
-#include "server_types.hpp"
+#include "raw_data_package_descriptor.hpp"
+#include "raw_data_package_reader.hpp"
+#include "raw_data_package_utils.hpp"
+#include "raw_data_package_writer.hpp"
+#include "stepper_host.hpp"
+#include "stepper_ipc_data_infra.hpp"
+#include "stepper_motor.hpp"
 
-#ifndef MSG_HEAD
-#   define MSG_HEAD "MSG_HEAD"
+#ifndef MSG_PREAMBLE
+#   error "MSG_PREAMBLE is not defined"
 #endif
 
-#ifndef MSG_TAIL
-#   define MSG_TAIL "MSG_TAIL"
+#ifndef MSG_SIZE_FIELD_LEN
+#   error "MSG_SIZE_FIELD_LEN is not defined"
 #endif
 
 #ifndef PICO_IPC_BAUD
-#   define PICO_IPC_BAUD 9600UL
+#   error "PICO_IPC_BAUD is not defined"
 #endif
 
-#ifndef PICO_IPC_MAX_BUFF_SIZE
-#   define PICO_IPC_MAX_BUFF_SIZE 1000UL
-#endif
-
-using namespace pico_mcu_ipc;
-using namespace server;
-using namespace cnc_server;
-using namespace server_utl;
-using namespace vendor;
+using namespace ipc;
+using namespace host;
 using namespace manager;
-using namespace pico_mcu_platform;
 
-static PicoSynchronousIpcConnection::Baud cast_baud(uint baud);
+static auto s_raw_data_buffer = RawData();
 
-static bool match(const RawData& data, const RawData& head, const RawData& tail);
-static Request extract(RawData *data, const RawData& head, const RawData& tail);
-static RawData serialize(const server::Response& response, const RawData& head, const RawData& tail);
-
-static Gpio *create_gpio(const Data& create_body);
-static void timeout(const double& timeout);
+static void generate_timeout(const std::size_t& timeout_ms);
+static StepperMotor *create_stepper();
+static void write_raw_data(const RawData& data);
+static void init_uart_listener();
 
 int main(void) {
+    const auto preamble_str = std::string(MSG_PREAMBLE);
+    const auto package_descriptor = RawDataPackageDescriptor(
+        RawData(preamble_str.begin(), preamble_str.end()),
+        MSG_SIZE_FIELD_LEN
+    );
+    const auto raw_data_reader = RawDataPackageReader(
+        &s_raw_data_buffer,
+        package_descriptor,
+        parse_package_size
+    );
+    const auto raw_data_writer = RawDataPackageWriter(
+        package_descriptor,
+        serialize_package_size,
+        write_raw_data
+    );
+    auto host = StepperHost(
+        raw_data_reader,
+        raw_data_writer,
+        create_stepper,
+        generate_timeout
+    );
+    
     stdio_init_all();
-
-    PicoSynchronousIpcConnection connection(
-        cast_baud(PICO_IPC_BAUD),
-        [](const server::Response& response) {
-            return serialize(response, MSG_HEAD, MSG_TAIL);
-        },
-        [](const RawData& data) {
-            return match(data, MSG_HEAD, MSG_TAIL);
-        },
-        [](RawData *data) {
-            return extract(data, MSG_HEAD, MSG_TAIL);
-        }
-    );
-
-    CncServer<std::string> server(
-        &connection,
-        "cnc_server",
-        create_gpio,
-        timeout
-    );
-
-    server.run();
+    init_uart_listener();
 
     while (true) {
-        connection.loop();
+        host.run_once();
     }
     return 0;
-}
-
-inline PicoSynchronousIpcConnection::Baud cast_baud(uint baud) {
-    switch (baud) {
-    case 9600UL:
-        return PicoSynchronousIpcConnection::Baud::B9600;
-    case 115200UL:
-        return PicoSynchronousIpcConnection::Baud::B115200;
-    default:
-        throw std::invalid_argument("unsupported baud received");
-    }
-}
-
-inline bool match(const RawData& data, const RawData& head, const RawData& tail) {
-    auto head_pos = data.find(head);
-    if (RawData::npos == head_pos) {
-        return false;
-    }
-    auto tail_pos = data.find(tail, head_pos + tail.size());
-    if (RawData::npos == tail_pos) {
-        return false;
-    }
-    return true;
-}
-
-inline Request extract(RawData *data, const RawData& head, const RawData& tail) {
-    auto head_pos = data->find(head);
-    if (RawData::npos == head_pos) {
-        throw std::invalid_argument("missing head");
-    }
-    auto tail_pos = data->find(tail, head_pos + head.size());
-    if (RawData::npos == tail_pos) {
-        throw std::invalid_argument("missing tail");
-    }
-    RawData extracted(data->begin() + head_pos + head.size(), data->begin() + tail_pos);
-    data->erase(data->begin() + head_pos, data->begin() + tail_pos + tail.size());
-    return JsonRequestParser()(extracted);
-}
-
-inline RawData serialize(const server::Response& response, const RawData& head, const RawData& tail) {
-    return head + JsonResponseSerializer()(response) + tail;
-}
-
-inline Gpio *create_gpio(const Data& create_cfg) {
-    const auto& cfg_obj(Data::cast<Object>(create_cfg));
-    const auto id(Data::cast<Integer>(cfg_obj.access("gpio_id")).get());
-    auto dir = static_cast<Gpio::Direction>(Data::cast<Integer>(cfg_obj.access("dir")).get());
-    switch (dir) {
-    case Gpio::Direction::IN:
-        return new PicoGpi(id);
-    case Gpio::Direction::OUT:
-        return new PicoGpo(id);
-    default:
-        throw ServerException(ResponseCode::BAD_REQUEST, "unsupported direction received");
-    }
-}
-
-inline void timeout(const double& timeout) {
-    const auto delay_us = static_cast<uint64_t>(static_cast<float>(1000000) * timeout);
-    sleep_us(delay_us);
 }
