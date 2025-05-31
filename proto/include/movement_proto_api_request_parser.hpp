0d8ab18d365396f0126f2dd06d0a20211e427876
fedddot <idanrommm@gmail.com>
2025-05-31 15:49:35 +0000

    config req parsed

--- proto/include/movement_proto_api_request_parser.hpp
+++ proto/include/movement_proto_api_request_parser.hpp
@@ -3,15 +3,17 @@
 
 #include <stdexcept>
 
-#include "linear_movement_request.hpp"
-#include "movement_manager_vector.hpp"
-#include "pb.h"
 #include "pb_decode.h"
 
+#include "axes_controller_config_request.hpp"
+#include "axis_config.hpp"
 #include "ipc_data.hpp"
 #include "ipc_instance.hpp"
+#include "linear_movement_request.hpp"
+#include "movement_manager_data.hpp"
 #include "movement_vendor_api.pb.h"
 #include "movement_vendor_api_request.hpp"
+#include "pico_axis_controller_config.hpp"
 #include "rotation_movement_request.hpp"
 
 namespace ipc {
@@ -27,6 +29,8 @@ namespace ipc {
         static Instance<vendor::MovementVendorApiRequest> parse_config_request(const movement_vendor_api_MovementApiRequest& pb_request);
         static Instance<vendor::MovementVendorApiRequest> parse_linear_movement_request(const movement_vendor_api_MovementApiRequest& pb_request);
         static Instance<vendor::MovementVendorApiRequest> parse_rotational_movement_request(const movement_vendor_api_MovementApiRequest& pb_request);
+        static pico::AxisConfig parse_axis_config(const movement_vendor_api_AxisConfig& pb_axis_config);
+        static pico::AxisConfig::DirectionsMapping parse_directions_mapping(const movement_vendor_api_DirectionsMapping& pb_directions);
     };
 
     inline Instance<vendor::MovementVendorApiRequest> MovementProtoApiRequestParser::operator()(const RawData& raw_data) const {
@@ -49,7 +53,17 @@ namespace ipc {
     }
 
     inline Instance<vendor::MovementVendorApiRequest> MovementProtoApiRequestParser::parse_config_request(const movement_vendor_api_MovementApiRequest& pb_request) {
-        throw std::runtime_error("ConfigRequest parsing is not implemented yet");
+        const auto& pb_cfg_request = pb_request.request.config_request;
+        if (!pb_cfg_request.has_axes_config) {
+            throw std::invalid_argument("AxesControllerConfigApiRequest is missing axes configuration");
+        }
+        const auto& pb_axes_config = pb_cfg_request.axes_config;
+        const auto axes_config = pico::PicoAxesControllerConfig::AxesConfig {
+            {manager::Axis::X, parse_axis_config(pb_axes_config.x_axis_cfg)},
+            {manager::Axis::Y, parse_axis_config(pb_axes_config.y_axis_cfg)},
+            {manager::Axis::Z, parse_axis_config(pb_axes_config.z_axis_cfg)},
+        };
+        return Instance<vendor::MovementVendorApiRequest>(new vendor::AxesControllerConfigApiRequest<pico::PicoAxesControllerConfig>(axes_config));
     }
 
     inline Instance<vendor::MovementVendorApiRequest> MovementProtoApiRequestParser::parse_linear_movement_request(const movement_vendor_api_MovementApiRequest& pb_request) {
@@ -87,6 +101,36 @@ namespace ipc {
         const auto speed = static_cast<double>(pb_rotation_request.speed);
         const auto angle = static_cast<double>(pb_rotation_request.angle);
         return Instance<vendor::MovementVendorApiRequest>(new vendor::RotationMovementRequest(target, rotation_center, angle, speed));
+    }
+
+    inline pico::AxisConfig MovementProtoApiRequestParser::parse_axis_config(const movement_vendor_api_AxisConfig& pb_axis_config) {
+        if (!pb_axis_config.has_stepper_config) {
+            throw std::invalid_argument("AxisConfig is missing stepper configuration");
+        }
+        if (!pb_axis_config.has_directions_mapping) {
+            throw std::invalid_argument("AxisConfig is missing directions mapping");
+        }
+        return pico::AxisConfig(
+            pico::PicoStepper::Config {
+                .enable_pin = pb_axis_config.stepper_config.enable_pin,
+                .step_pin = pb_axis_config.stepper_config.step_pin,
+                .dir_pin = pb_axis_config.stepper_config.dir_pin,
+                .hold_time_us = pb_axis_config.stepper_config.hold_time_us
+            },
+            static_cast<double>(pb_axis_config.step_length),
+            parse_directions_mapping(pb_axis_config.directions_mapping)
+        );
+    }
+
+    inline pico::AxisConfig::DirectionsMapping MovementProtoApiRequestParser::parse_directions_mapping(const movement_vendor_api_DirectionsMapping& pb_directions) {
+        const auto directions_mapping = std::map<movement_vendor_api_StepperDirection, pico::PicoStepper::Direction> {
+            {movement_vendor_api_StepperDirection_CW, pico::PicoStepper::Direction::CW},
+            {movement_vendor_api_StepperDirection_CCW, pico::PicoStepper::Direction::CCW},
+        };
+        return pico::AxisConfig::DirectionsMapping {
+            { manager::Direction::NEGATIVE, directions_mapping.at(pb_directions.negative) },
+            { manager::Direction::POSITIVE, directions_mapping.at(pb_directions.positive) },
+        };
     }
 }
 
