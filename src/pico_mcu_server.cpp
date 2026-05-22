4c2284694d2da9db0a1344865fc929688b85fb47
fedddot <idanrommm@gmail.com>
2026-05-22 06:29:39 +0000

    Refactor pico_mcu_server to use FatFs for filesystem operations and update CMake configuration for library linking

--- src/pico_mcu_server.cpp
+++ src/pico_mcu_server.cpp
@@ -1,22 +1,11 @@
 #include <cstring>
-
-extern "C" {
-    #include "sd_io.h"
-}
-
+#include "ff.h"
 int main(void) {
-    SD_DEV sd_dev;
-    std::memset(&sd_dev, 0, sizeof(sd_dev));
-    volatile const auto init_status = SD_Init(&sd_dev);
-    if (SD_OK != init_status) {
-        return -1;
-    }
-    char buf[512];
-    volatile auto res = SD_Read(&sd_dev, buf, 0, 0, 512);
-    res = SD_Read(&sd_dev, buf, 462, 0, 512);
-    res = SD_Read(&sd_dev, buf, 463, 0, 512);
-    res = SD_Read(&sd_dev, buf, 464, 0, 512);
-    res = SD_Read(&sd_dev, buf, 465, 0, 512);
+    FATFS fs;
+    std::memset(&fs, 0, sizeof(fs));
+
+    f_mount(&fs, "0", 0);   
+    
     while (true) {
     }
     return 0;
