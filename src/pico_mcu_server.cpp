e1f14258625d59cdda4a69166b7bf6569f2aeb66
fedddot <idanrommm@gmail.com>
2026-05-23 09:52:43 +0000

    Refactor SD card file handling in pico_mcu_server to write data instead of reading, ensuring proper file creation and closure

--- src/pico_mcu_server.cpp
+++ src/pico_mcu_server.cpp
@@ -7,21 +7,24 @@ int main(void) {
     FATFS fs;
     std::memset(&fs, 0, sizeof(fs));
 
-    if (FRESULT::FR_OK != f_mount(&fs, "0:", 0)) {
+    if (FRESULT::FR_OK != f_mount(&fs, "0:", 1)) {
         throw std::runtime_error("Failed to mount SD card");
     }
 
     FIL file;
-    if (FRESULT::FR_OK != f_open(&file, "0:test.md", FA_READ)) {
+    if (FRESULT::FR_OK != f_open(&file, "0:test.md", FA_CREATE_ALWAYS | FA_WRITE)) {
         throw std::runtime_error("Failed to open file on SD card");
     }
-    char data[20UL] = { '\0' };
-    UINT bytes_read(0);
-    if (FRESULT::FR_OK != f_read(&file, data, sizeof(data), &bytes_read)) {
+    char data[] = "onetwothree";
+    UINT bytes_write(0);
+    if (FRESULT::FR_OK != f_write(&file, data, sizeof(data), &bytes_write)) {
         throw std::runtime_error("Failed to read from file on SD card");
     }
     if (FRESULT::FR_OK != f_close(&file)) {
         throw std::runtime_error("Failed to close file on SD card");
+    }
+    if (FRESULT::FR_OK != f_unmount("0:")) {
+        throw std::runtime_error("Failed to unmount SD card");
     }
 
     while (true) {
