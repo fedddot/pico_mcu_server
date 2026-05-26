2d217ad0699dcd3b4c1a84c48721604e09c1dd50
fedddot <idanrommm@gmail.com>
2026-05-26 17:04:54 +0000

    Add command enumeration and send_command method to SdSpiDriver class

--- sd_spi_driver/include/sd_spi_driver.hpp
+++ sd_spi_driver/include/sd_spi_driver.hpp
@@ -45,6 +45,60 @@ namespace sd_spi_driver {
         TrancieveByte m_trancieve_byte;
         Delay m_delay;
         ChipSelector m_chip_selector;
+
+        enum class SdCommand: std::uint8_t {
+            CMD0 = 0x40 + 0,
+            CMD1 = 0x40 + 1,
+            CMD8 = 0x40 + 8,
+            CMD9 = 0x40 + 9,
+            CMD16 = 0x40 + 16,
+            CMD17 = 0x40 + 17,
+            CMD24 = 0x40 + 24,
+            CMD42 = 0x40 + 42,
+            CMD55 = 0x40 + 55,
+            CMD58 = 0x40 + 58,
+            ACMD41 = 0x40 + 41
+        };
+
+        static std::uint8_t calculate_crc(const SdCommand cmd, std::uint32_t arg) {
+            (void)arg;
+            enum: std::uint8_t {
+                DUMMY_CRC = 0x01,
+                CMD0_CRC = 0x95,
+                CMD8_CRC = 0x87
+            };
+            switch (cmd) {
+            case SdCommand::CMD0:
+                return CMD0_CRC;
+            case SdCommand::CMD8:
+                return CMD8_CRC;
+            default:
+                return DUMMY_CRC;
+            }
+        }
+
+        std::uint8_t send_command(const SdCommand cmd, std::uint32_t arg) {
+            m_chip_selector(ChipSelectState::SELECTED);
+            m_trancieve_byte(cmd);
+            m_trancieve_byte((std::uint8_t)(arg >> 24));
+            m_trancieve_byte((std::uint8_t)(arg >> 16));
+            m_trancieve_byte((std::uint8_t)(arg >> 8 ));
+            m_trancieve_byte((std::uint8_t)(arg >> 0 ));
+            m_trancieve_byte(calculate_crc(cmd, arg));
+
+            enum: std::size_t { ATTEMPTS_NUMBER = 50UL };
+            auto attempts = ATTEMPTS_NUMBER;
+            std::uint8_t res(0xFF);
+            while (attempts) {
+                res = m_trancieve_byte(0xFF);
+                if (res != 0xFF) {
+                    break;
+                }
+                --attempts;
+            }
+            m_chip_selector(ChipSelectState::UNSELECTED);
+            return res;
+        }
     };
 }
 
